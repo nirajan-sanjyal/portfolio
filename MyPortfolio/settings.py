f0541fc0f0b2afd4d3b6f8ad08c3f52f6efcc6ed
nirajan-sanjyal <sanjyalnirajan6@gmail.com>
2026-06-15 12:07:22 +0545

    commit

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 
 # Email (for contact form)
@@ -146,6 +145,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
