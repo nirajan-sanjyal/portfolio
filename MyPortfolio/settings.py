a9aee7c6bb81178bf1dd339233fed5354d2cbd67
nirajan-sanjyal <sanjyalnirajan6@gmail.com>
2026-06-15 12:45:09 +0545

    commit

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -61,10 +61,11 @@ ROOT_URLCONF = 'MyPortfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
